a9260f13516fc7d53373de49d56a6b25cd7429a6
Oksana Astapova <astapova.frontend@gmail.com>
2022-08-10 01:03:52 +0300

    fix table winners

--- src/ts/winners.ts
+++ src/ts/winners.ts
@@ -1,5 +1,9 @@
 import { winners } from "./main";
 
+declare global {
+    interface Window { toggle_up: any; }
+    interface Window { toggle_up_win: any; }
+}
 
 export async function get_winners(){
 
@@ -10,9 +14,86 @@ const dataCars = async () => {
     };
         
     const cars = await dataCars();
+    createTable(cars);
+    const time = document.querySelector('#time');
+    const win = document.querySelector('#wins');
+
+    window.toggle_up = toggle_up;
+    window.toggle_up_win = toggle_up_win;
+
+    function toggle_up_win(){
+        win.classList.toggle('time-up');
+        if(win.classList.contains('time-up')){
+                       createTable(cars)
+            const img_up= document.querySelector('#up-win') as HTMLBodyElement;
+
+            img_up.style.display = 'flex';
+        }else{
+            createTable(cars)
+            
+            const img_down= document.querySelector('#down-win') as HTMLBodyElement;
+                  img_down.style.display = 'flex';
+        }
+    }
+    function toggle_up(): void{
+        time.classList.toggle('time-up');
+
+        if(time.classList.contains('time-up')){
+                  cars.sort(function (a: { time: number; }, b: { time: number; }) {
+                    if (a.time > b.time) {
+                      return 1;
+                    }
+                    if (a.time < b.time) {
+                      return -1;
+                    }
+                    return 0;
+                  });
+                  createTable(cars)
+                  const img_up= document.querySelector('#up') as HTMLBodyElement;
+                  img_up.style.display = 'flex';
+        }else{
+            cars.sort(function (a: { time: number; }, b: { time: number; }) {
+                if (a.time < b.time) {
+                  return 1;
+                }
+                if (a.time > b.time) {
+                  return -1;
+                }
+                return 0;
+              });
+              createTable(cars)
+              const img_down= document.querySelector('#down') as HTMLBodyElement;
+                  img_down.style.display = 'flex';
+        }
+        
+    }
+
+    // time.classList.add('time-down');
+    // time.addEventListener('click', ()=>{
+    //     time.classList.toggle('time-up')
+
+    //     if(time.classList.contains('time-up')){
+    //       let cars_sort_up = cars.sort(function (a: { time: number; }, b: { time: number; }) {
+    //         if (a.time > b.time) {
+    //           return 1;
+    //         }
+    //         if (a.time < b.time) {
+    //           return -1;
+    //         }
+    //         return 0;
+    //       });
+    //       createTable(cars_sort_up)
+    //         const time = document.querySelector('#time');
+          
+    //         time.classList.add('time-up') 
+    //     }
+    // })
+}
+
+function createTable(cars: any){
     let table = document.querySelector('.table');
     table.innerHTML =`<tr class = 'header-table'>
-        <th>N</th><th>Winner</th><th>Wins</th><th>Time</th>
+        <th>N</th><th>Winner</th><th id='wins' onclick='toggle_up_win()'><p>win</p><img id='up-win' src='up.png'><img id='down-win'src='down.png'><th id='time' onclick='toggle_up()'><p>Time</p><img id='up' src='up.png'><img id='down'src='down.png'></th>
        </tr>`;
         
     for (let i = 0; i < cars.length; i++) {
@@ -20,10 +101,9 @@ const dataCars = async () => {
 
         const title = document.querySelector('.winners__title h1');
         title.innerHTML = `Winners (${cars.length})`
-        
 
         const row = `<tr class = 'row'>
-        <td>${i+1}</td><td>${car.name}</td><td>${car.wins}</td><td>${car.time}</td>
+        <td>${i+1}</td><td>${car.name}</td><td>${car.wins}</td><td class='time'>${car.time}</td>
        </tr>`
 
        table.innerHTML += row;
